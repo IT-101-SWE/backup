09d31616d790f39b5c8beb9996d1bb9237f1d68f
احمد محمد عمر محمد <128517751+Ahmed20225120@users.noreply.github.com>
2023-04-22 13:08:39 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,102 +1,71 @@
+//flag to control error message box
+var oflag = 0;
 
 window.onload = () =>
 {
+
     //back button
     const backBtn = document.getElementById("back_btn");
     //previous position
     let position = document.referrer;
-
     backBtn.onclick = () =>
-    {   if (position.includes("err"))
-        {
-            window.history.go(-2);
-        }
-        else
+    {
+        if(position.includes("Home"))
         {
             window.history.go(-1);
         }
+        else
+        {
+            window.history.go(-2);
+        }
     }
 
+    //direct to sign up page
+
+    const sign_upBtn = document.getElementById("sign_up");
+
+    sign_upBtn.onclick = () =>
+    {
+
+        window.location.href = "../Sign_Up/";
+    }
     
     //define error box (var) is used to set then to global
     let err_box = document.getElementById("error_box");
     window.onclick = () =>
     {
-        err_box.style.display = "none";
+        err_box.style.display = 'none'
+
     }
-    
+    validInput(oflag);
+
+
     
 
 }
 
-//password validation function 
-//flag is set in argument to let php use it 
+//password validation function
+//flag is set in argument to let php use it
 function validInput(flag=0)
 {
-    let pass1 = document.getElementById("password1");
-    let pass2 = document.getElementById("password2");
-    //store password1 value in variable
-    let passwordChar = pass1.value;
-    
+
     //define error box (var) is used to set then to global
     let err_box = document.getElementById("error_box");
-    //error message 
-    let err = " ";
 
-    
-    
+    //error message
+    let err = "Invalid Email OR Password !";
 
-    //check for special characters
-    if(!( (passwordChar.includes('@')) || (passwordChar.includes('!')) || (passwordChar.includes('#')) || (passwordChar.includes('^')) || (passwordChar.includes('$')) || (passwordChar.includes('%')) || (passwordChar.includes('&')) || (passwordChar.includes('-')) || (passwordChar.includes('*')) || (passwordChar.includes('_'))))
-    {
-        flag = 4;
-    }
-
-    //check first character if capital
-    if( !(passwordChar[0] >= 'A' && passwordChar[0]<= 'Z') )
-    {
-        flag = 1;
-    }
-
-    //check for length of the password
-    if(!(passwordChar.length > 8))
-    {
-        flag = 3;
-    }
-
-
-    //check matching
-    if(!(pass1.value == pass2.value))
-    {
-        pass1.value = "";
-        pass2.value = "";
-        flag = 2;
-    }   
-    
     //check which condition is false
-    if(flag != 0)
+    if(flag == 1)
     {
-        switch(flag)
-        {
-            case 1:
-                err = "First Letter in password Must be capital !";
-                break;
-            case 2:
-                err = "passwords Not matching !";
-                break;
-            case 3:
-                err = "password lenght must be more than 8 characters !";
-                break;
-            case 4:
-                err = "password Must include special characrter !";
-                break;
-        }
-        err_box.style.display = 'inline-block';
-        err_box.style.justifyContent = "center";
+
+        err_box.style.display  = 'inline-block';
+        err_box.style.justifyContent = 'center';
         err_box.innerHTML = err + "<br><br><br> ( click on screen around error window to continue )";
-        
-        return false;
+
     }
-    
-    return true;
+    else
+    {
+        err_box.style.display = 'none';
+    }
 }
